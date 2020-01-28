71eb28e12c4ec4167d10e9ae9548731e739b3b57
Okami <okamihorokeu@gmail.com>
2020-01-28 13:19:41 -0300

    arreglado el puerto segundo intento

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const app = require('./app');
 
+
 app.set('port', process.env.PORT || 3000);
 
 
 async function main() {
-    await app.listen('port');
+    await app.listen(app.get('port'));
     console.log('Server is running')
 }
 
