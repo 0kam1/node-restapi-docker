f6ae918b70867d8886d189f0101bd34f41ad2035
Okami <okamihorokeu@gmail.com>
2020-01-28 13:12:39 -0300

    cambiando el puerto de escucha

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const app = require('./app');
 
+app.set('port', process.env.PORT || 3000);
+
+
 async function main() {
-    await app.listen(3000);
+    await app.listen('port');
     console.log('Server is running')
 }
 
